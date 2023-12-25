85f395e82bd616671b2da8455588f48c3b5a11f4
Thiago Sales <thiagosales2026@gmail.com>
2023-12-25 19:57:24 -0300

    fox1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Feed from './Feed';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  console.log(articles);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getArticles();
+  }, [])
+
+
+  console.log(articles);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='text-xl font-semibold
+      text-center mt-5'>RSS Feed</h1>
+      <h2 className='text-xl font-semibold
+      text-center mt-2 mb-4'>Good morning, Thiago</h2>
+      <div className='w-3/4 max-w-lg border mx-auto
+      p-5 rounded-xl'>
+        <div className='bg-black flex flex-col 
+        items-center rounded-lg py-3 px-6 mb-5'>
+          <img src='https://cdn-images-1.medium.com/v2/resize:fit:606/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png'
+          alt='Netflix technology blog rss feed'/>
+
+        </div>
+      {articles.map((item, i) => 
+        <Feed 
+          key={i}
+          title={item.item.title}
+          link={item.item.link}
+          date={item.item.pubDate}
+
+        />
+      )}
+      </div>
+    </>
   );
 }
 
---
+++ src/Feed.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Feed = ({ title, link, date }) => {
+
+    let formatted = { day:"numeric", month: "long", year: "numeric" }
+    let articleDate = new Date(date).toLocaleDateString("pt-br", formatted);
+
+  return (
+    <>
+        <a href={link} target='_blank' rel='noopener noreferrer'
+        className='hover:opacity-70 
+        hover:text-orange-500'>
+            <h3 className='text-xl mb-1 
+            underline'>{title}</h3>
+            <p>{articleDate}</p>
+        </a>
+    </>
+  )
+}
+
+export default Feed
